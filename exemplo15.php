4b7afadf09454f6edfb9b1e6ae88f00a07ab3c57
Miguel <miguel.t.carvalho@aluno.senai.br>
2026-04-15 09:05:39 -0300

    atividades 15

---
+++ exemplo15.php
@@ -0,0 +1,82 @@
+<?php
+class Conta
+{
+    public $numero;
+    public $saldo;
+
+    function __construct($numero, $saldo)
+    {
+        $this->numero = $numero;
+        $this->saldo = $saldo;
+    }
+
+    function creditar($credito)
+    {
+        $this->saldo = $this->saldo + $credito;;
+    }
+
+
+
+    function debitar($credito)
+    {
+        $this->saldo = $this->saldo - $credito;
+    }
+
+    function transferir($outraConta, $valor)
+    {
+        if ($this->saldo > $valor) {
+            $this->debitar($valor);
+            $outraConta->creditar($valor);
+        }
+    }
+
+
+
+
+}
+
+class Poupanca extends Conta
+{
+
+    public $juros;
+
+    function __construct($numero, $saldo, $juros)
+    {
+        parent::__construct($numero, $saldo);
+        $this->juros = $juros;
+
+    }
+    function creditar($valor)
+    {
+        parent::creditar($valor);
+        $this->atualizaJuros();
+
+    }
+
+    function atualizaJuros()
+    {
+
+    $this->saldo = $this->saldo * (1+ $this->juros) ;
+    }
+
+
+
+
+}
+
+$conta = new Conta( 1, 150);
+$conta -> creditar(50);
+$conta -> debitar(50);
+echo "Saldo da conta {$conta->numero} é igual a: {$conta->saldo}";
+
+$poupanca = new Poupanca(2, 150, 0.10);
+$poupanca -> creditar(50);
+$poupanca -> creditar(100);
+$poupanca-> atualizaJuros();
+
+echo "Saldo da conta {$poupanca->numero} é igual a: {$poupanca->saldo}";
+
+
+
+
+
